a01a9637e0ee99aa37462746fe704fe6a9bb4a2d
RoseK <RoseK@uavdit01dnp0004.GMCC.GRANGE.LOCAL>
2022-07-27 12:45:56 -0400

    First commit

---
+++ src/test/java/com/grangeinsurance/fortuneteller/FortunetellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grangeinsurance.fortuneteller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortunetellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
