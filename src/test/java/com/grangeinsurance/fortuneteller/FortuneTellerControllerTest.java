3fb3a48c3f912f062f271fe995ec4ef076b5a3b1
RoseK <RoseK@uavdit01dnp0004.GMCC.GRANGE.LOCAL>
2022-07-29 13:54:12 -0400

    Unit tests pass for fortuneTellerController

--- src/test/java/com/grangeinsurance/fortuneteller/FortuneTellerControllerTest.java
+++ src/test/java/com/grangeinsurance/fortuneteller/FortuneTellerControllerTest.java
@@ -1,5 +1,37 @@
 package com.grangeinsurance.fortuneteller;
 
-public class FortuneTellerControllerTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class FortuneTellerControllerTest {
+
+@Mock FortuneTellerService mockService;
+	
+	private FortuneTellerController subject;
+	
+	@BeforeEach
+	void setup() {
+		subject = new FortuneTellerController(mockService);
+	}
+	
+	@Test
+	void fortunesControllerInitialized() {
+		assertThat(subject).isNotNull();
+	}
+	
+	@Test
+	void controllerReturnsRandomFortune() {
+		final String expected = "Test";
+		
+		when(mockService.findMyFate()).thenReturn(expected);
+		String response = subject.findMyFateMethod();
+	    assertThat(response.getClass().getSimpleName()).isEqualTo(expected.getClass().getSimpleName());
+	}
 }
